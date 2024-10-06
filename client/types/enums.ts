583c1de1a0d333ce67701c4a679f46fcd7bd7598
jhnmcndn <johnmcondino@gmail.com>
2024-10-06 21:02:59 +0800

    chore: integrate typings and enums

---
+++ client/types/enums.ts
@@ -0,0 +1,14 @@
+export enum Status {
+  Todo = 'To Do',
+  WorkInProgress = 'Work in Progress',
+  UnderReview = 'Under Review',
+  Completed = 'Completed',
+}
+
+export enum Priority {
+  Urgent = 'Urgent',
+  High = 'High',
+  Medium = 'Medium',
+  Low = 'Low',
+  BackLog = 'Back Log',
+}
---
+++ client/types/types.ts
@@ -0,0 +1,46 @@
+import { Priority, Status } from '@/types/enums';
+
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface User {
+  userId?: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  cognitoId?: string;
+  teamId?: number;
+}
+
+export interface Attachment {
+  id: number;
+  fileUrl: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
