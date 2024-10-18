0d0ee3f11f8c46f48d01ff28c66409807089a494
jhnmcndn <johnmcondino@gmail.com>
2024-10-18 22:28:35 +0800

    feat: server integrate backend for search page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import projectRoutes from "./routes/projectRoutes";
-import taskRoutes from "./routes/taskRoutes";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import projectRoutes from './routes/projectRoutes';
+import taskRoutes from './routes/taskRoutes';
+import searchRoutes from './routes/searchRoutes';
 /* ROUTE IMPORTS */
 
 /* CONFIGURATIONS */
@@ -13,22 +14,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get("/", (req, res) => {
-  res.send("This is home route");
+app.get('/', (req, res) => {
+  res.send('This is home route');
 });
 
 app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
+app.use('/search', searchRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on part ${port}`);
 });
