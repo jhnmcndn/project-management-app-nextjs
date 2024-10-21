e807195231a8d81eb5a925c6aa0e2ec79b932775
jhnmcndn <johnmcondino@gmail.com>
2024-10-21 22:40:45 +0800

    feat: api initial for priority

--- client/state/api.ts
+++ client/state/api.ts
@@ -25,6 +25,11 @@ export const api = createApi({
           ? result.map(({ id }) => ({ type: 'Tasks' as const, id }))
           : [{ type: 'Tasks' as const }],
     }),
+    getTasksByUser: build.query<Task[], number>({
+      query: (userId) => `tasks/user/${userId}`,
+      providesTags: (result, error, userId) =>
+        result ? result.map(({ id }) => ({ type: 'Tasks', id })) : [{ type: 'Tasks', id: userId }],
+    }),
     createTask: build.mutation<Task, Partial<Task>>({
       query: (task) => ({
         url: 'tasks',
@@ -64,4 +69,5 @@ export const {
   useSearchQuery,
   useGetUsersQuery,
   useGetTeamsQuery,
+  useGetTasksByUserQuery,
 } = api;
