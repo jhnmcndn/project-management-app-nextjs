8af3e284f7a8b4eac0ee85aa3b11f0299124c993
jhnmcndn <johnmcondino@gmail.com>
2024-10-22 15:36:26 +0800

    feat: add ecosystem config for aws

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'project-management-app-nextjs',
+      script: 'npm',
+      args: 'run dev',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
