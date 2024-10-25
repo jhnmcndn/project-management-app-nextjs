c0133b3fa64ae83da28d8295c6f7ed5b35686269
jhnmcndn <johnmcondino@gmail.com>
2024-10-25 18:55:21 +0800

    style: fix spacing

--- client/app/components/Providers/authProvider.tsx
+++ client/app/components/Providers/authProvider.tsx
@@ -42,7 +42,7 @@ const formFields = {
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='mt-5'>
+    <div className='pt-5'>
       <Authenticator formFields={formFields}>
         {({ user }) =>
           user ? (
